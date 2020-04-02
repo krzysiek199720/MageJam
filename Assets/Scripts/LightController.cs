69bb01674ad0add98bf8088d6e73c0ce9d55d8b3
Krzysztof Staniszewski <krzysiek199720@gmail.com>
2020-04-02 23:11:07 +0200

    LWRP, lighthingy, fog

---
+++ Assets/Scripts/LightController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightController : MonoBehaviour
+{
+    public float walkSpeed = 2f;
+    public float lightRadius = 1f;
+    private Rigidbody2D rigidbody2d;
+
+    private Vector2 moveDir;
+    void Start()
+    {
+        moveDir = Vector2.zero;
+        rigidbody2d = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        Vector2 move = Vector2.zero;
+        //INPUTS
+        if (Input.GetKey(KeyCode.W)) move.y = 1;
+        if (Input.GetKey(KeyCode.S)) move.y = -1;
+        if (Input.GetKey(KeyCode.A)) move.x = -1;
+        if (Input.GetKey(KeyCode.D)) move.x = 1;
+
+        moveDir = move.normalized;
+    }
+    private void FixedUpdate()
+    {
+        rigidbody2d.MovePosition(Vector2.MoveTowards(rigidbody2d.position, rigidbody2d.position + moveDir, walkSpeed * Time.fixedDeltaTime));
+
+    }
+}
