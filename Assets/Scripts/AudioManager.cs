f182d76cc2a2db3c73802cd527bb18a6cc05c012
Mateusz Wdowiak <55950450+matija00@users.noreply.github.com>
2020-04-06 16:51:01 +0200

    main menu

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager instance;
+    public Sound[] sounds;
+
+    private void Awake()
+    {
+        instance = GetComponent<AudioManager>();
+
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+        }    
+    }
+
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+
+        if (!IsSound || s == null)
+            return;
+
+        s.source.Play();
+    }
+
+    public static bool IsSound
+    {
+        get
+        {
+            return PlayerPrefs.GetInt("isSound", 1) == 1;
+        }
+        set
+        {
+            PlayerPrefs.SetInt("isSound", value ? 1 : 0);
+        }
+    }
+}
---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public GameObject tutorialPanel;
+
+    public void StartGame()
+    {
+        StartCoroutine("LoadGame");
+    }
+
+    IEnumerator LoadGame()
+    {
+        CanvasGroup canvasGroup = tutorialPanel.GetComponent<CanvasGroup>();
+
+        canvasGroup.interactable = true;
+        canvasGroup.blocksRaycasts = true;
+        canvasGroup.alpha = 1;
+
+        yield return new WaitForSeconds(3f);
+        SceneManager.LoadScene(1);
+    }
+
+    public void SkipScene()
+    {
+        SceneManager.LoadScene(1); 
+    }
+
+}
